ef0b29101825aee719672092012537f3323c7127
Jyben <benjamin.letessier@gmail.com>
2017-05-03 19:35:20 +0200

    Notification issue
    We now send notifications to all players.

--- server/sv_emergency.lua
+++ server/sv_emergency.lua
@@ -10,12 +10,18 @@
 RegisterServerEvent('es_em:sendEmergency')
 AddEventHandler('es_em:sendEmergency',
   function(reason, playerIDInComa, x, y, z)
-    TriggerClientEvent('es_em:sendEmergencyToDocs', source, reason, playerIDInComa, x, y, z)
+    local players = GetPlayers()
+    for k, v in pairs(players) do
+      TriggerClientEvent('es_em:sendEmergencyToDocs', v, reason, playerIDInComa, x, y, z)
+    end
   end
 )
 
 RegisterServerEvent('es_em:getTheCall')
 AddEventHandler('es_em:getTheCall',
   function(playerName, playerID)
-    TriggerClientEvent('es_em:callTaken', source, playerName, playerID)
+    local players = GetPlayers()
+    for k, v in pairs(players) do
+      TriggerClientEvent('es_em:callTaken', v, playerName, playerID)
+    end
   end)
