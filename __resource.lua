255f1a6decc659f910eab9ebfdd3f123d3affae2
Jyben <benjamin.letessier@gmail.com>
2017-05-03 19:34:09 +0200

    Ressource update
    Changing the path

--- __resource.lua
+++ __resource.lua
@@ -5,7 +5,7 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 dependency 'essentialmode'
 
 -- Emergency
-client_script 'emergency/client/cl_healthplayer.lua'
-client_script 'emergency/client/cl_emergency.lua'
+client_script 'client/cl_healthplayer.lua'
+client_script 'client/cl_emergency.lua'
 
-server_script 'emergency/server/sv_emergency.lua'
+server_script 'server/sv_emergency.lua'
