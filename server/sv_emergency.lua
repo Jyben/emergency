6df423b431d0934b44217e3a7bc9e882c7194504
Jyben <benjamin.letessier@gmail.com>
2017-05-05 03:25:49 +0200

    Issue #2
    Fix notifications
    Adding new event to rez player

--- server/sv_emergency.lua
+++ server/sv_emergency.lua
@@ -10,9 +10,9 @@
 RegisterServerEvent('es_em:sendEmergency')
 AddEventHandler('es_em:sendEmergency',
   function(reason, playerIDInComa, x, y, z)
-    local players = GetPlayers()
-    for k, v in pairs(players) do
-      TriggerClientEvent('es_em:sendEmergencyToDocs', v, reason, playerIDInComa, x, y, z)
+    print('emergency')
+    for v = 0, 31, 1 do
+      TriggerClientEvent('es_em:sendEmergencyToDocs', v, reason, playerIDInComa, x, y, z, source)
     end
   end
 )
@@ -20,8 +20,17 @@ AddEventHandler('es_em:sendEmergency',
 RegisterServerEvent('es_em:getTheCall')
 AddEventHandler('es_em:getTheCall',
   function(playerName, playerID)
-    local players = GetPlayers()
-    for k, v in pairs(players) do
+    print('getthecall')
+    for v = 0, 31, 1 do
       TriggerClientEvent('es_em:callTaken', v, playerName, playerID)
     end
-  end)
+  end
+)
+
+RegisterServerEvent('es_em:sv_resurectPlayer')
+AddEventHandler('es_em:sv_resurectPlayer',
+  function(sourcePlayerInComa)
+    print('res')
+    TriggerClientEvent('es_em:cl_resurectPlayer', sourcePlayerInComa)
+  end
+)
