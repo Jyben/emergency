c2f037b365edba665a21fd8b0291feb57c6880c3
Jyben <benjamin.letessier@gmail.com>
2017-05-21 17:09:25 +0200

    Send multiple calls
    If the emergencies do not respond, you can now call them again every 15 seconds until you receive a call.

--- client/cl_healthplayer.lua
+++ client/cl_healthplayer.lua
@@ -50,6 +50,7 @@ local txt = {
 local isDead = false
 local isKO = false
 local isRes = false
+local emergencyComes = false
 
 --[[
 ################################
@@ -142,6 +143,7 @@ AddEventHandler('baseevents:onPlayerKilled',
 RegisterNetEvent('es_em:cl_sendMessageToPlayerInComa')
 AddEventHandler('es_em:cl_sendMessageToPlayerInComa',
 	function()
+		emergencyComes = true
 		SendNotification(txt[lang]['ambIsComming'])
 	end
 )
@@ -218,13 +220,16 @@ function OnPlayerDied(playerId, reasonID, reason)
 	Citizen.CreateThread(
 		function()
 			local emergencyCalled = false
+			local notifReceivedAt = nil
 
 			while not isRes do
 				Citizen.Wait(1)
+
 				if (IsControlJustReleased(1, Keys['E'])) and not emergencyCalled then
 					if not isDocConnected then
 						ResPlayer()
 					else
+						notifReceivedAt = GetGameTimer()
 						SendNotification(txt[lang]['youCallAmb'])
 						TriggerServerEvent('es_em:sendEmergency', reason, GetPlayerServerId(PlayerId()), pos.x, pos.y, pos.z)
 					end
@@ -233,10 +238,17 @@ function OnPlayerDied(playerId, reasonID, reason)
 				elseif (IsControlJustReleased(1, Keys['X'])) then
 					ResPlayer()
 				end
+
+				if (GetTimeDifference(GetGameTimer(), notifReceivedAt) > 15000) and not emergencyComes and emergencyCalled then
+					SendNotification(txt[lang]['callAmb'])
+					emergencyCalled = false
+				end
+
 			end
 
 			isDocConnected = nil
 			isRes = false
+			emergencyComes = false
 	end)
 end
 
